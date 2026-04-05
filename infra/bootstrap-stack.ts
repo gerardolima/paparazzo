f45288576ef74a29f0968bbdc387f5cca1fd9da4
Gerardo Lima <gerardo.lima@gmail.com>
2026-04-05 13:15:48 +0100

    chore(infra): set limits to ECR and logs

--- infra/bootstrap-stack.ts
+++ infra/bootstrap-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib'
 import * as iam from 'aws-cdk-lib/aws-iam'
+import * as cr from 'aws-cdk-lib/custom-resources'
 import type { Construct } from 'constructs'
 
 export class BootstrapStack extends cdk.Stack {
@@ -33,5 +34,40 @@ export class BootstrapStack extends cdk.Stack {
     role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'))
 
     new cdk.CfnOutput(this, 'GitHubActionsRoleArn', { value: role.roleArn })
+
+    // ------------------------------------------------------------------------
+    // ecr: lifecycle policy on CDK bootstrap repository
+    // ------------------------------------------------------------------------
+    const bootstrapEcrRepoName = `cdk-hnb659fds-container-assets-${this.account}-${this.region}`
+
+    new cr.AwsCustomResource(this, 'EcrLifecyclePolicy', {
+      onCreate: {
+        service: 'ECR',
+        action: 'putLifecyclePolicy',
+        parameters: {
+          repositoryName: bootstrapEcrRepoName,
+          lifecyclePolicyText: JSON.stringify({
+            rules: [
+              {
+                rulePriority: 1,
+                selection: {
+                  tagStatus: 'any',
+                  countType: 'imageCountMoreThan',
+                  countNumber: 3,
+                },
+                action: { type: 'expire' },
+              },
+            ],
+          }),
+        },
+        physicalResourceId: cr.PhysicalResourceId.of('ecr-lifecycle-policy'),
+      },
+      policy: cr.AwsCustomResourcePolicy.fromStatements([
+        new iam.PolicyStatement({
+          actions: ['ecr:PutLifecyclePolicy'],
+          resources: [`arn:aws:ecr:${this.region}:${this.account}:repository/${bootstrapEcrRepoName}`],
+        }),
+      ]),
+    })
   }
 }
--- infra/paparazzo-stack.ts
+++ infra/paparazzo-stack.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib'
 import * as events from 'aws-cdk-lib/aws-events'
 import * as targets from 'aws-cdk-lib/aws-events-targets'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as logs from 'aws-cdk-lib/aws-logs'
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as ssm from 'aws-cdk-lib/aws-ssm'
 import type { Construct } from 'constructs'
@@ -45,6 +46,7 @@ export class PaparazzoStack extends cdk.Stack {
       memorySize: 2048, // 2GB
       ephemeralStorageSize: cdk.Size.gibibytes(2),
       timeout: cdk.Duration.minutes(15),
+      logRetention: logs.RetentionDays.TWO_WEEKS,
       environment: {
         S3_BUCKET: bucket.bucketName,
         SSM_API_KEY_NAME: '/paparazzo/google-api-key',
